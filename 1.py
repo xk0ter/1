3e8c94a35d1f0f1d577431d640b4ff68fbe4d539
xk0ter <xkoter@yandex.ru>
2026-01-30 13:50:42 +0800

    [-o-]

---
+++ 1.py
@@ -0,0 +1,112 @@
+"""
+class Employee:
+    def init(self, name, position):
+        self.name = name
+        self.position = position
+
+    def display_info(self):
+        print(f"Сотрудник: {self.name}, Должность: {self.position}")
+
+if name == "main":
+    emp1 = Employee("Иван Петров", "Менеджер")
+    emp2 = Employee("Анна Сидорова", "Разработчик")
+
+    emp1.display_info()
+    emp2.display_info()
+"""
+
+
+class Book:
+    def init(self, name, author, year):
+        self.name = name
+        self.author = author
+        self.year = year
+
+    def display_info(self):
+        print(f"Название: {self.name}, Автор: {self.author}, Год выпуска: {self.year}")
+if name == "main":
+    book1 = Book("Война и мир", "Лев Толстой", 1869)
+    book2 = Book("1984", "Джордж Оруэлл", 1949)
+    book3 = Book("Мастер и Маргарита", "Михаил Булгаков", 1966)
+    book1.display_info()
+    book2.display_info()
+    book3.display_info()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
