47275fba968d123fd696eb10c942c0d1b4f8aba6
xk0ter <xkoter@yandex.ru>
2026-02-20 10:15:24 +0800

    [-o-]

---
+++ mibombo.py
@@ -0,0 +1,158 @@
+
+"""for i in range(1,10+1):
+    print(i)
+"""
+from idlelib.autocomplete import TRY_A
+
+from setuptools.unicode_utils import try_encode
+
+"""
+for i in range(2,20+2,2):
+    print(i)
+"""
+"""
+n = int(input())
+sum = 0
+for i in range(1,n+1):
+    sum += i
+print(f"Сумма всех чисел от 1 до {n} равна: {sum}")
+"""
+"""
+a = int(input())
+for i in range(1,11):
+    b = a * i
+    print(f"{a} * {i} = {b}")
+"""
+"""
+n = int(input())
+sum = 1
+for i in range(1,n+1):
+    sum *= i
+print(f"{n} равна: {sum}")
+"""
+"""
+for i in range(10,1,-1):
+    print(i)
+
+print("старт")
+"""
+"""
+import random
+a = random.randint(1,10)
+while True:
+    b = int(input())
+    if b == a:
+        print("congratulation")
+        break
+    else:
+        print("not congratulation")
+"""
+"""
+for i in range(1,11):
+    print(i ** 2)
+"""
+"""
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+e = int(input())
+for i in a,b,c,d,e:
+    if i < 999999:
+        print(i)
+        break
+"""
+"""
+a = int(input())
+has_passport = True
+if has_passport == True and a >= 18:
+    print("можно")
+else:
+    print("нельзя")
+"""
+"""
+a = input("введите логин")
+b = input("введите пароль")
+if a == "admin" and b == 1234 or a == "user" and b == "pass":
+    print("доступ разррешен")
+else:
+    print("запрещен")
+"""
+"""
+a = int(input())
+if a == 6 and 7:
+    print("выходные")
+else:
+    print("будни")
+"""
+"""
+a = int(input())
+if 10 <= a <= 20 and a == 0:
+    print("dddd")
+"""
+"""
+license = bool(input())
+a = int(input())
+if a >= 21 and license == True:
+    print("mojno")
+else:
+    print(("незя"))
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
