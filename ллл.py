d2e1d82c0abdb9dcc5b92bbbe4c559afc1f117d4
xk0ter <xkoter@yandex.ru>
2026-03-27 04:16:57 +0800

    222
    222

---
+++ ллл.py
@@ -0,0 +1,40 @@
+def greet(name):
+    print(f"привет {name}")
+
+def sum_two(a, b):
+    return a + b
+
+def is_even(n):
+    return n % 2 == 0
+
+def max(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
+
+def string_length(s):
+    return len(s)
+
+def rectangle_area(a, b):
+    return a * b
+
+def square(n):
+    return n * n
+
+def celsius_to_fahrenheit(c):
+    return c * 9/5 + 32
+
+
+
+
+
+
+
+
+
+
+
+
